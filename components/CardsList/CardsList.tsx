8b95b0967dda3ba419eebadcee2b79b13db93f09
elgiz20002000 <92743270+elgiz20002000@users.noreply.github.com>
2023-02-14 11:01:26 +0400

    first push

--- components/CardsList/CardsList.tsx
+++ components/CardsList/CardsList.tsx
@@ -7,25 +7,29 @@ import { getSavedMovies } from '../../Store/SaveSlice'
 
 const CardsList = () => {
   const router = useRouter()
-  let movies 
+  let  movies = useSelector(getMovies) 
+  let savedMovies = useSelector(getSavedMovies)
 
-  if(router.pathname == '/Saved') {
-    movies = useSelector(getSavedMovies)
-  } else {
-    movies = useSelector(getMovies)
-  }
+  
 
   console.log(movies)
   
   return <>
         <div className={styles.cards_list}>
-            {movies ? movies.map(( {Poster , Title , imdbID})  => {
+          {router.pathname != '/Saved' ? movies ? movies.map(( {Poster , Title , imdbID})  => {
               if(Poster !== 'N/A') {
                 return <Card id={imdbID} key={imdbID} img={Poster} title={Title}/>
               } else {
                 return
               }
-            } ) : 'Not Found' }
+            } ) : 'Not Found'  : savedMovies ? savedMovies.map(( {Poster , Title , imdbID})  => {
+              if(Poster !== 'N/A') {
+                return <Card id={imdbID} key={imdbID} img={Poster} title={Title}/>
+              } else {
+                return
+              }
+            } ) : 'Not Found' } 
+            
       </div>
   </>
 }
