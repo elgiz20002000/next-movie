7e4b5c5164f0a33822b509ea7bc50c8c6213eb68
elgiz20002000 <92743270+elgiz20002000@users.noreply.github.com>
2023-02-15 14:01:05 +0400

    first push

--- components/Spinner/Spinner.jsx
+++ components/Spinner/Spinner.jsx
@@ -1,17 +1,18 @@
-import Image from "next/image"
-import style from './Spinner.module.scss'
+import Image from "next/image";
+import style from "./Spinner.module.scss";
 
-const Spinner = ({width , height}) => {
+const Spinner = ({ width, height }) => {
   return (
     <div className={style.loading}>
-        <Image
-         src={'/gif/loading.gif'}
-         alt='loading'
-         width={width}
-         height={height}   
-        />
+      <Image
+        unoptimized
+        src={"/gif/loading.gif"}
+        alt="loading"
+        width={width}
+        height={height}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Spinner
+export default Spinner;
