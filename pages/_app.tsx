1f18a1c6cb31dd2ef5960c7e7bb9871afac122c8
elgiz20002000 <92743270+elgiz20002000@users.noreply.github.com>
2023-02-15 13:11:27 +0400

    first push

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ import { SessionProvider } from "next-auth/react";
 
 function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={session} basePath="/api/auth">
+    <SessionProvider session={session}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
